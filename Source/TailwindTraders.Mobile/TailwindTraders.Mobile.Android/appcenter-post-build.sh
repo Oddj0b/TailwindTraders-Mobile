bb0ad50e159450c921ba0c1500dbddb99c8cbcfa
Villars Gimm <villars.gimm@gmail.com>
2020-05-27 17:07:47 +0200

    ls nuget folder

--- Source/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-post-build.sh
+++ Source/TailwindTraders.Mobile/TailwindTraders.Mobile.Android/appcenter-post-build.sh
@@ -42,7 +42,7 @@ DEFAULT_DEVICES="7c5a701f"
 UI_TEST_TOOLS_DIR=$APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools
 
 echo "Debug output: Finding test-cloud.exe"
-ls $UI_TEST_TOOLS_DIR
+ls /Users/vsts/.nuget/packages/xamarin.uitest/2.2.7/tools
 find $UI_TEST_TOOLS_DIR -name 'test-cloud.exe' -exec echo{} \; 
 
 if [ -z "$APPCENTER_TOKEN" ]; then
